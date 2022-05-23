600b4223983187bb434ed3d3e1ade510c450fe7e
Denis Panin <deniswork2021@ukr.net>
2022-05-23 20:49:39 +0300

    added models and registered on adminpanel

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Actor, Genre, MovieShots, RatingStar, Rating, Reviews, Movie
 
-# Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Category)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(MovieShots)
+admin.site.register(RatingStar)
+admin.site.register(Rating)
+admin.site.register(Reviews)
+
